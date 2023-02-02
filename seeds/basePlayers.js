257c325b81a21058855eb1f5a448774fcf7eadda
akosilar <55524614+akosilar@users.noreply.github.com>
2023-02-01 23:22:42 -0500

    updated player seeds
    based on top 27 ITTF rankings

--- seeds/basePlayers.js
+++ seeds/basePlayers.js
@@ -1,14 +1,164 @@
 module.exports = [
     {
-        firstName: 'Koki',
-        lastName: 'Niwa',
-        email: 'kn@alibaba.com',
-        rating: 2700
+        firstName: 'FAN',
+        lastName: 'Zhendong',
+        email: 'fzd@weibo.com',
+        rating: 7725
     },
     {
-        firstName: 'Ma',
+        firstName: 'MA',
         lastName: 'Long',
-        email: 'ml@prott',
-        rating: 2703
+        email: 'ml@weibo.com',
+        rating: 4400
+    },
+    {
+        firstName: 'WANG',
+        lastName: 'Chuqin',
+        email: 'wcq@weibo.com',
+        rating: 4345
+    },
+    {
+        firstName: 'Tomokazu',
+        lastName: 'HARIMOTO',
+        email: 'th@yahoo.jp',
+        rating: 3860
+    },
+    {
+        firstName: 'Hugo',
+        lastName: 'Calderano',
+        email: 'hg@live.com',
+        rating: 3525
+    },
+    {
+        firstName: 'Truls',
+        lastName: 'MOREGARD',
+        email: 'tl@mail.com',
+        rating: 3345
+    },
+    {
+        firstName: 'LIANG',
+        lastName: 'Jingkun',
+        email: 'ljk@weibo.com',
+        rating: 2985
+    },
+    {
+        firstName: 'LIN',
+        lastName: 'Yun-Ju',
+        email: 'lyj@weibo.com',
+        rating: 2555
+    },
+    {
+        firstName: 'Darko',
+        lastName: 'JORGIC',
+        email: 'dj@email.si',
+        rating: 2250
+    },
+    {
+        firstName: 'Dang',
+        lastName: 'QIU',
+        email: 'dq@web.de',
+        rating: 2140
+    },
+    {
+        firstName: 'Dimitrij',
+        lastName: 'OVTCHAROV',
+        email: 'do@web.de',
+        rating: 1905
+    },
+    {
+        firstName: 'LIN',
+        lastName: 'Gaoyuan',
+        email: 'lgy@weibo.com',
+        rating: 1845
+    },
+    {
+        firstName: 'Patrick',
+        lastName: 'FRANZISKA',
+        email: 'pf@web.de',
+        rating: 1780
+    },
+    {
+        firstName: 'Quadri',
+        lastName: 'ARUNA',
+        email: 'qa@truehost.com.ng',
+        rating: 1745
+    },
+    {
+        firstName: 'Timo',
+        lastName: 'BOLL',
+        email: 'tb@web.de',
+        rating: 1565
+    },
+    {
+        firstName: 'LIM',
+        lastName: 'Jonghoon',
+        email: 'lj@naver.kr',
+        rating: 1555
+    },
+    {
+        firstName: 'CHUANG',
+        lastName: 'Chih-Yuan',
+        email: 'ccy@weibo.com',
+        rating: 1495
+    },
+    {
+        firstName: 'JANG',
+        lastName: 'Woojin',
+        email: 'jw@naver.kr',
+        rating: 1490
+    },
+    {
+        firstName: 'Anton',
+        lastName: 'KALLBERG',
+        email: 'ak@mail.com',
+        rating: 1175
+    },
+    {
+        firstName: 'Kanak',
+        lastName: 'JHA',
+        email: 'kj@godaddy.com',
+        rating: 1175
+    },
+    {
+        firstName: 'Kristian',
+        lastName: 'KARLSSON',
+        email: 'kk@mail.com',
+        rating: 1160
+    },
+    {
+        firstName: 'Yukiya',
+        lastName: 'UDA',
+        email: 'yu@yahoo.com.jp',
+        rating: 1148
+    },
+    {
+        firstName: 'LEE',
+        lastName: 'Sangsu',
+        email: 'ls@naver.kr',
+        rating: 1045
+    },
+    {
+        firstName: 'Liam',
+        lastName: 'PITCHFORD',
+        email: 'lp@outlook.com',
+        rating: 1045
+    },
+    {
+        firstName: 'Omar',
+        lastName: 'ASSAR',
+        email: 'oa@amfws.com',
+        rating: 1005
+    },
+    {
+        firstName: 'Andrej',
+        lastName: 'GACINA',
+        email: 'ag@avalon.hr',
+        rating: 980
+    },
+    {
+        firstName: 'Alexis',
+        lastName: 'LEBRUN',
+        email: 'al@hotmail.fr',
+        rating: 953
     },
 ]
